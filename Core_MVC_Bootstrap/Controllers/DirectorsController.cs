fbf4d2f20b411a4f44f6f09559eda3524ec2ef84
Badhon-PC <Badhon@DESKTOP-OK3LNJL>
2023-01-03 00:54:52 +0600

    directorController created and package modified

---
+++ Core_MVC_Bootstrap/Controllers/DirectorsController.cs
@@ -0,0 +1,26 @@
+﻿using App.Home.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace App.Home.Controllers
+{
+    public class DirectorsController : Controller
+    {
+        private readonly MHDBContext _mHDBContext;
+        public DirectorsController(MHDBContext mHDBContext)
+        {
+            _mHDBContext = mHDBContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var demo = await _mHDBContext.TblDirectors.ToListAsync();
+            return View(demo);
+        }
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
