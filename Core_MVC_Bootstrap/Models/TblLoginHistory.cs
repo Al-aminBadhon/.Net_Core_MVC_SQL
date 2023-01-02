46e2e104b72a15a6bc9067d1de8bd3a95d2676d8
Badhon-PC <Badhon@DESKTOP-OK3LNJL>
2023-01-03 00:16:30 +0600

    MHDBContext Added & connectionString

---
+++ Core_MVC_Bootstrap/Models/TblLoginHistory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App.Home.Models
+{
+    public partial class TblLoginHistory
+    {
+        public int LogHisId { get; set; }
+        public int UserId { get; set; }
+        public DateTime LastLoginTime { get; set; }
+
+        public virtual TblUser User { get; set; }
+    }
+}
