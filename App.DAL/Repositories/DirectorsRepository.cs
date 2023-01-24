31f0477e7c31a7fed1c2022fcf122c72119ef605
Badhon-PC <Badhon@DESKTOP-OK3LNJL>
2023-01-25 01:11:35 +0600

    begin transaction added in directorController + BLL & DAL added

---
+++ App.DAL/Repositories/DirectorsRepository.cs
@@ -0,0 +1,18 @@
+﻿using App.DAL.Data;
+using App.DAL.RepositoryContracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.DAL.Repositories
+{
+    public class DirectorsRepository :IDirectorsRepository
+    {
+        private readonly MHDBContext _mHDBContext;
+        public DirectorsRepository(MHDBContext mHDBContext)
+        {
+            _mHDBContext = mHDBContext;
+        }
+
+    }
+}
